81420cec3821f0813b5e38e977a538399fabcdb6
Jay Khakim <mgmediajay@gmail.com>
2024-07-03 14:58:19 +0900

    FEAT: Prettier implemented

--- sorting/bubbleSort.js
+++ sorting/bubbleSort.js
@@ -1,19 +1,19 @@
 //ES2015
 
 //Optimized with noSwaps
-function buubleSort(arr){
-    let noSwaps;
-    const swap = (arr, indx1, indx2)=>{
-        [arr[indx1], arr[indx2]] = [arr[indx2], arr[indx1]]
-    }
-    for(let i=arr.length; i>0; i--){
-        noSwaps = true;
-        for(let j=0; j<i-1; j++){
-            if(arr[j]>arr[j+1]){
-               swap(arr, j, j+1)
-               noSwaps = false
-            }
-        }
-        if(noSwaps) break;
-    }
+function buubleSort(arr) {
+	let noSwaps;
+	const swap = (arr, indx1, indx2) => {
+		[arr[indx1], arr[indx2]] = [arr[indx2], arr[indx1]];
+	};
+	for (let i = arr.length; i > 0; i--) {
+		noSwaps = true;
+		for (let j = 0; j < i - 1; j++) {
+			if (arr[j] > arr[j + 1]) {
+				swap(arr, j, j + 1);
+				noSwaps = false;
+			}
+		}
+		if (noSwaps) break;
+	}
 }
--- sorting/selectionSort.js
+++ sorting/selectionSort.js
@@ -1,18 +1,17 @@
-function selectionSort(arr){
-    const swap = (arr, indx1, indx2)=>{
-        [arr[indx1], arr[indx2]] = [arr[indx2], arr[indx1]]
-    }
-    for (let i=0; i<arr.length; i++){
-        var lowest = i; 
-        for(let j=i+1; j<arr.length; j++){
-            if(arr[j] < arr[lowest]){
-                lowest = j
-            }
-        }
-        swap(arr, i, lowest)
-
-    }
-    return arr; 
+function selectionSort(arr) {
+	const swap = (arr, indx1, indx2) => {
+		[arr[indx1], arr[indx2]] = [arr[indx2], arr[indx1]];
+	};
+	for (let i = 0; i < arr.length; i++) {
+		var lowest = i;
+		for (let j = i + 1; j < arr.length; j++) {
+			if (arr[j] < arr[lowest]) {
+				lowest = j;
+			}
+		}
+		swap(arr, i, lowest);
+	}
+	return arr;
 }
 
-console.log(selectionSort([1, 5, 3, 10, 56, 33]))
+console.log(selectionSort([1, 5, 3, 10, 56, 33]));
