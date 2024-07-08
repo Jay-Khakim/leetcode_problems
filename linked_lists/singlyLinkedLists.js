25642aa6513298fedd0b3a2f4b7b9c3cda1ae134
Jay Khakim <mgmediajay@gmail.com>
2024-07-08 19:13:50 +0900

    Singly Linked Lists are doen

--- linked_lists/singlyLinkedLists.js
+++ linked_lists/singlyLinkedLists.js
@@ -53,14 +53,121 @@ class SinglyLinkedList {
 		}
 		return current;
 	}
+
+	// removing the node from the head
+	shift() {
+		if (!this.head) return undefined;
+		let currentHead = this.head;
+		this.head = currentHead.next;
+		this.length--;
+		if (this.length === 0) {
+			this.head = null;
+			this.tail = null;
+		}
+		return currentHead;
+	}
+
+	//Adding new node to the head
+	unshifting(val) {
+		let newNode = new Node(val);
+		if (!this.head) {
+			this.head = newNode;
+			this.tail = this.head;
+		} else {
+			newNode.next = this.head;
+			this.head = newNode;
+		}
+		this.length++;
+		return this;
+	}
+
+	//getting teh value of node with given index
+	get(index) {
+		if (index < 0 || index >= this.length) return null;
+		let count = 0;
+		let current = this.head;
+		while (count !== index) {
+			current = current.next;
+			count++;
+		}
+		return current;
+	}
+
+	//taking one seppcific node and changing its value
+	set(index, val) {
+		let foundNode = this.get(index);
+		if (foundNode) {
+			foundNode.val = val;
+			return true;
+		}
+		return false;
+	}
+
+	//iserting new value to specific index
+	insert(index, val) {
+		if (index < 0 || index > this.length) return false;
+		if (index === this.length) return !!this.push(val);
+		if (index === 0) return !!this.unshifting(val);
+
+		let newNode = new Node(val);
+		let prev = this.get(index - 1);
+		let temp = prev.next;
+		prev.next = newNode;
+		newNode.next = temp;
+		this.length++;
+		return true;
+	}
+
+	//removing the one node of specific index
+	remove(index) {
+		if (index < 0 || index >= this.length) return undefined;
+		if (index === 0) return this.shift();
+		if (index === this.length - 1) return this.pop();
+		let previousNode = this.get(index - 1);
+		let removed = previousNode.next;
+		previousNode.next = removed.next;
+		this.length--;
+		return removed;
+	}
+
+	reverse() {
+		let node = this.head;
+		this.head = this.tail;
+		this.tail = node;
+		let next;
+		let prev = null;
+		for (let i = 0; i < this.length; i++) {
+			next = node.next;
+			node.next = prev;
+			prev = node; 
+			node = next;
+		}
+		return this
+	}
+
+	//Printing all nodes in one array
+	print() {
+		let arr = [];
+		let current = this.head;
+		while (current) {
+			arr.push(current.val);
+			current = current.next;
+		}
+		console.log(arr);
+	}
 }
 
 let list = new SinglyLinkedList();
-list.push("Hello");
-list.push("World");
-list.push(99);
-list.pop();
-list.pop();
-list.pop();
-
 console.log(list);
+
+console.log("----- Insertion ----------------");
+console.log(list.insert(0, "Kahkimjonovs"));
+console.log(list.insert(1, "Kahkimjonovs"));
+console.log(list.insert(1, "Khakims"));
+console.log(list.insert(2, "You are good"));
+console.log(list.push("Cool"));
+// console.log(list);
+console.log(list.remove(0));
+console.log(list.print());
+console.log(list.reverse())
+console.log(list.print());
