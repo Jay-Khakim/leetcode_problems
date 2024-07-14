b86c7c5cf27c6d68d0803c12e5e2c94fde245bf1
Jay Khakim <mgmediajay@gmail.com>
2024-07-15 06:15:45 +0900

    SQL problems

--- leetcode/sql/sql_50.sql
+++ leetcode/sql/sql_50.sql
@@ -229,6 +229,40 @@ ON fo.customer_id = d.customer_id AND fo.min_date=d.order_date
 
 
 
+-- 1141. User Activity for the Past 30 Days I
+-- Write a solution to find the daily active user count for a period of 30 days ending 2019-07-27 inclusively.
+--  A user was active on someday if they made at least one activity on that day.
+
+-- Return the result table in any order.
+
+-- The result format is in the following example.
+SELECT activity_date AS day, count(DISTINCT user_id) AS active_users
+FROM Activity
+WHERE activity_date BETWEEN DATE('2019-06-28') AND DATE('2019-07-27')
+GROUP BY activity_date
+ORDER BY activity_date;
+
+
+
+
+
+
+-- 596. Classes More Than 5 Students
+-- Write a solution to find all the classes that have at least five students.
+
+-- Return the result table in any order.
+
+-- The result format is in the following example.
+
+
+
+SELECT class 
+FROM Courses 
+GROUP BY class 
+HAVING COUNT(DISTINCT student) >= 5;
+
+
+
 
 -- 550. Game Play Analysis IV
 -- Write a solution to report the fraction of players that logged in again on the day after the day they first logged in, rounded to 2 decimal places. In other words, you need to count the number of players that logged in for at least two consecutive days starting from their first login date, then divide that number by the total number of players.
