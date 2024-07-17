f6728c246ba628faa8ed8c55ef1d77186dbf7ae3
Jay Khakim <mgmediajay@gmail.com>
2024-07-17 16:04:04 +0900

    Stack and ueues

---
+++ stack_queue/stack.js
@@ -0,0 +1,46 @@
+class Node {
+	constructor(value) {
+		this.value = value;
+		this.next = null;
+	}
+}
+
+class Stack {
+	constructor() {
+		this.first = null;
+		this.last = null;
+		this.size = 0;
+	}
+
+	push(value) {
+		let newNode = new Node(value);
+		if (!this.first) {
+			this.first = newNode;
+			this.last = newNode;
+		} else {
+			let temp = this.first;
+			this.first = newNode;
+			this.first.next = temp;
+		}
+		return ++this.size;
+	}
+
+	pop() {
+		if (!this.first) return null;
+		let temp = this.first;
+		if (this.first === this.last) {
+			this.last = null;
+		}
+		this.first = this.first.next;
+		this.size--;
+		return temp.value;
+	}
+}
+let stack = new Stack();
+
+stack.push(11);
+stack.push(112);
+stack.push(114);
+stack.push(1141010);
+console.log(stack.pop())
+console.log(stack);
