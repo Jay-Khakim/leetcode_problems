3c53b59d26a1f706172bafd6bdb61dc11e975732
Jay Khakim <mgmediajay@gmail.com>
2024-07-18 14:26:30 +0900

    Binary Trees Search

--- search_algorithms/binary_search/binaryPreSudoCode.js
+++ search_algorithms/binary_search/binaryPreSudoCode.js
@@ -2,22 +2,21 @@
 // Write a function called binarySearch which accepts a sorted array and a value and returns the index at which the value exists.
 // Otherwise, return -1.
 
-function binarySearch(arr, val){
-    let left  = 0
-     let right = arr.length -1 
-     
- 
-     while(left <= right){
-         let middle = Math.floor((left + right)/2)
- 
-         if(val === arr[middle]){
-             return middle
-         } else if( val < arr[middle]){
-             right = middle - 1
-         }else {
-             left = middle + 1
-         }
-     }
- 
-     return -1 
- }
+function binarySearch(arr, val) {
+	let left = 0;
+	let right = arr.length - 1;
+
+	while (left <= right) {
+		let middle = Math.floor((left + right) / 2);
+
+		if (val === arr[middle]) {
+			return middle;
+		} else if (val < arr[middle]) {
+			right = middle - 1;
+		} else {
+			left = middle + 1;
+		}
+	}
+
+	return -1;
+}
---
+++ trees/binaryTreesSearch.js
@@ -0,0 +1,88 @@
+class Node {
+	constructor(value) {
+		this.value = value;
+		this.left = null;
+		this.right = null;
+	}
+}
+
+class BST {
+	constructor() {
+		this.root = null;
+	}
+
+	insert(value) {
+		let newNode = new Node(value);
+		if (this.root === null) {
+			this.root = newNode;
+			return this;
+		}
+		let current = this.root;
+		while (true) {
+			if (value === current.value) return undefined;
+			if (value < current.value) {
+				if (current.left === null) {
+					current.left = newNode;
+					return this;
+				}
+				current = current.left;
+			} else {
+				if (current.right === null) {
+					current.right = newNode;
+					return this;
+				}
+				current = current.right;
+			}
+		}
+	}
+
+        //Finding the value and returing it
+	find(value) {
+		if (this.root === null) return false;
+		let current = this.root;
+		let found = false;
+		while (current && !found) {
+			if (value < current.value) {
+				current = current.left;
+			} else if (value > current.value) {
+				current = current.right;
+			}else{
+                                found = true;
+                        }
+		}
+                if(!found) return undefined
+                return current;
+	}
+
+        //Finding the value and returning treu or false
+        contains(value) {
+		if (this.root === null) return false;
+		let current = this.root;
+		let found = false;
+		while (current && !found) {
+			if (value < current.value) {
+				current = current.left;
+			} else if (value > current.value) {
+				current = current.right;
+			}else{
+                               return true
+                        }
+		}
+                return false;
+	}
+}
+
+let tree = new BST();
+// tree.root = new Node(10)
+// tree.root.right = new Node(15)
+// tree.root.left = new Node(7)
+// tree.root.left.right = new Node(9)
+
+tree.insert(10);
+tree.insert(5);
+tree.insert(13);
+tree.insert(2);
+tree.insert(7);
+tree.insert(7);
+
+console.log(tree.contains(23));
