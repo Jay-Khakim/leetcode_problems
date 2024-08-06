4af877a3995e5b5892933307cc3500e89718318b
Jay Khakim <mgmediajay@gmail.com>
2024-08-06 18:08:41 +0900

    Leetcode problems

--- trees/binaryTreesSearch.js
+++ trees/binaryTreesSearch.js
@@ -1,138 +1,20 @@
-class Node {
-	constructor(value) {
-		this.value = value;
-		this.left = null;
-		this.right = null;
+var minDepth = function(root) {
+  
+	if (root === null) {
+	  return 0;
 	}
-}
-
-class BST {
-	constructor() {
-		this.root = null;
+      
+	if (root.left === null && root.right === null) {
+	  return 1;
 	}
-
-	insert(value) {
-		let newNode = new Node(value);
-		if (this.root === null) {
-			this.root = newNode;
-			return this;
-		}
-		let current = this.root;
-		while (true) {
-			if (value === current.value) return undefined;
-			if (value < current.value) {
-				if (current.left === null) {
-					current.left = newNode;
-					return this;
-				}
-				current = current.left;
-			} else {
-				if (current.right === null) {
-					current.right = newNode;
-					return this;
-				}
-				current = current.right;
-			}
-		}
+      
+	if (root.left === null) {
+	  return minDepth(root.right) + 1;
 	}
-
-	//Finding the value and returing it
-	find(value) {
-		if (this.root === null) return false;
-		let current = this.root;
-		let found = false;
-		while (current && !found) {
-			if (value < current.value) {
-				current = current.left;
-			} else if (value > current.value) {
-				current = current.right;
-			} else {
-				found = true;
-			}
-		}
-		if (!found) return undefined;
-		return current;
+      
+	if (root.right === null) {
+	  return minDepth(root.left) + 1;
 	}
-
-	//Finding the value and returning treu or false
-	contains(value) {
-		if (this.root === null) return false;
-		let current = this.root;
-		let found = false;
-		while (current && !found) {
-			if (value < current.value) {
-				current = current.left;
-			} else if (value > current.value) {
-				current = current.right;
-			} else {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	BFS() {
-		let data = [];
-		let queue = [];
-		let node = this.root;
-		queue.push(node);
-		while (queue.length) {
-			node = queue.shift();
-			data.push(node.value);
-			if (node.left) queue.push(node.left);
-			if (node.right) queue.push(node.right);
-		}
-		return data;
-	}
-
-	DFSPreOrder() {
-		let data = [];
-		function traverse(node) {
-			data.push(node.value);
-			if (node.left) traverse(node.left);
-			if (node.right) traverse(node.right);
-		}
-		traverse(this.root);
-		return data;
-	}
-
-	DFSPostOrder() {
-		let data = [];
-		function traverse(node) {
-			if (node.left) traverse(node.left);
-			if (node.right) traverse(node.right);
-			data.push(node.value);
-		}
-		traverse(this.root);
-		return data;
-	}
-
-	DFSInOrder() {
-		let data = [];
-		function traverse(node) {
-			if (node.left) traverse(node.left);
-			data.push(node.value);
-			if (node.right) traverse(node.right);
-		}
-		traverse(this.root);
-		return data;
-	}
-}
-
-let tree = new BST();
-// tree.root = new Node(10)
-// tree.root.right = new Node(15)
-// tree.root.left = new Node(7)
-// tree.root.left.right = new Node(9)
-
-tree.insert(10);
-tree.insert(6);
-tree.insert(15);
-tree.insert(3);
-tree.insert(8);
-tree.insert(20);
-
-console.log("BFS: " + tree.BFS());
-console.log("DFS PreOrder: " + tree.DFSPreOrder());
-console.log("DFS PostOrder: " + tree.DFSPostOrder());
-console.log("DFS InOrder: " + tree.DFSInOrder());
+      
+	return Math.min(minDepth(root.left), minDepth(root.right)) + 1;  
+      };
